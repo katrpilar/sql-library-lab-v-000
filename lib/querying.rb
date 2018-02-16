eadcbf2ab19a65bf9ca46026540007a66e4ef16c
Katrina Brinson <kbrinso@gmail.com>
2018-02-16 21:34:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,10 +20,16 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
+<<<<<<< HEAD
   "SELECT authors.name, subgenres.name
   FROM series
     INNER JOIN authors ON authors.id = series.author_id
     INNER JOIN subgenres ON subgenres.id = series.subgenre_id;"
+=======
+  "SELECT (SELECT authors.name FROM authors LEFT JOIN series ON authors.id = series.author_id),
+  (SELECT subgenres.name FROM subgenres LEFT JOIN series ON subgenres.id = series.subgenre_id)
+  FROM series"
+>>>>>>> 872ef327692899ada3a11c0945f78ae87d751743
 end
 
 def select_series_title_with_most_human_characters
@@ -37,6 +43,10 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
   FROM characters
   INNER JOIN character_books ON characters.id = character_books.character_id
+<<<<<<< HEAD
   GROUP BY characters.name
   ORDER BY COUNT(character_books.book_id) DESC"
+=======
+  GROUP BY characters.name"
+>>>>>>> 872ef327692899ada3a11c0945f78ae87d751743
 end
